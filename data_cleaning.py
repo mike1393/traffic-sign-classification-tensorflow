a769dba39e7842aebb53eaad703e0be952af7796
mike1393 <57059936+mike1393@users.noreply.github.com>
2022-04-16 18:17:00 +0800

    Modified the example script to perform more general task
    The script does following thing
    1. Creates a new folder for data training and validation.
    2. Copy and split the old training data to new training and validation folder.
    3. Reorganized the testing data into corresponding classes.

--- data_cleaning.py
+++ data_cleaning.py
@@ -2,19 +2,21 @@
 #built-in
 import os
 #local
-from utils import train_val_split, create_if_not_found
+from utils import train_val_split, order_test_set, create_if_not_found
 
 if __name__ == "__main__":
-    #Get the full path of data folder
+    # Preparing Validation data
+    # Split the data from old training folder to new training and validation folder
     path_to_data = os.path.join(os.getcwd(), "data")
-    #Get the full path of origin training folder
     path_to_train = os.path.join(path_to_data,"Train")
-    #Create the path of new training folder
     path_to_new_train = os.path.join(os.path.join(path_to_data,"training_data"), "train")
     create_if_not_found(path_to_new_train)
-    #Create the path of new validation folder
     path_to_new_val = os.path.join(os.path.join(path_to_data,"training_data"), "val")
     create_if_not_found(path_to_new_val)
-    #Split the data from old training folder to new training and validation folder
     train_val_split(path_to_train=path_to_train, path_to_new_train=path_to_new_train, path_to_new_val=path_to_new_val)
     
+    # Preparing Testing Data
+    # Reorganize test images into corresponding class folders
+    path_to_test = os.path.join(path_to_data, "Test")
+    path_to_test_csv = os.path.join(path_to_data,"Test.csv")
+    order_test_set(path_to_test=path_to_test, path_to_test_csv=path_to_test_csv)
